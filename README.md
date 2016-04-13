39fe2ce9d544b8bf1e8d434d9708577289b44fdf
liuayong <404842799@qq.com>
2016-04-13 19:23:37 +0800

    Yii2博客

---
+++ README.md
@@ -0,0 +1,5 @@
+a blog demo for yii2.0
+
+Yii2blog是用来配合《魏曦的Yii2.0视频教程》http://www.weixistyle.com 进行讲解的例子。
+
+Yii2blog是一个博客系统，需求是参照了Yii1.1里面非常经典的blog demo，然后用Yii2.0来进行了改写， 功能和代码都还有改进的空间，欢迎有兴趣的朋友一起来完善这个博客系统。
---
+++ config/db.php
@@ -0,0 +1,11 @@
+<?php
+
+//
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=blog2',
+    'username' => 'root',
+    'password' => '123123',
+    
+    'charset' => 'utf8',
+];
